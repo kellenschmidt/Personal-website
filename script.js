d20721d5c886e35d1d00bf75542e6f66bd1cf3e2
Kellen Schmidt <kellenschmidt31@gmail.com>
2017-03-08 23:16:27 -0600

    Updated navbar and scrolling, added footer and main content

---
+++ script.js
@@ -0,0 +1,22 @@
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 800);
+        return false;
+      }
+    }
+  });
+});
+
+$(function () {
+  $(document).scroll(function () {
+    var $nav = $(".navbar-fixed-top");
+    var $titlePage = $("#title-page");
+    $nav.toggleClass('scrolled', $(this).scrollTop() > $titlePage.height()-$nav.height());
+  });
+});
