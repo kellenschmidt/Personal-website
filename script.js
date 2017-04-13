9a39221555069c9c314c1fb4cb837594050d55d2
Kellen Schmidt <kellenschmidt31@gmail.com>
2017-04-12 22:28:21 -0500

    New scroll animation on click and AOS integration

--- script.js
+++ script.js
@@ -51,7 +51,7 @@ $(document).ready(function () {
 var $window = $(window);
 var element = $('section#test');
 // Percentage of viewport size that element is positioned down from top of viewport
-var heightFromViewportPct = element[0].getBoundingClientRect().top / $window.height();
+// var heightFromViewportPct = element[0].getBoundingClientRect().top / $window.height();
 // Percentage of height of viewport that element takes up
 var elementHeightPct = element.height() / $window.height();
 // Percentage of viewport to fade element at (0.85 of distance from top of viewport to top of element)
@@ -66,3 +66,45 @@ function test() {
 function test2() {
   $('#test').toggleClass("myFadeUpOut");
 }
+
+function isElementInViewport(elem) {
+  var $elem = $(elem);
+
+  // Get the scroll position of the page.
+  var scrollElem = ((navigator.userAgent.toLowerCase().indexOf('webkit') != -1) ? 'body' : 'html');
+  var viewportTop = $(scrollElem).scrollTop();
+  var viewportBottom = viewportTop + $(window).height();
+
+  // Get the position of the element on the page.
+  var elemTop = Math.round($elem.offset().top);
+  var elemBottom = elemTop + $elem.height();
+
+  return ((elemTop < viewportBottom) && (elemBottom > viewportTop));
+}
+
+// Check if it's time to start the animation.
+function checkAnimation() {
+  var $elem = $('#test');
+
+  // If the animation has already been started
+  // if ($elem.hasClass('myFadeUpIn')) return;
+
+  if (isElementInViewport($elem)) {
+    // Start the animation
+    $elem.addClass('myFadeUpIn');
+  } else {
+    $elem.removeClass('myFadeUpIn');
+  }
+}
+
+// Capture scroll events
+$(window).scroll(function () {
+  var scrollVar = $(window).scrollTop();
+  checkAnimation();
+});
+
+AOS.init({
+  easing: 'ease-out',
+  duration: 800,
+  offset: 400,
+});
